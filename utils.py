5d12a0a230b1ea8ac293c4219b79629fce7a4ba8
SavSV <sv.ework@gmail.com>
2023-01-28 13:01:21 +0300

    docstring added

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+"""
+Functions for text generation
+"""
+
 import torch
 from streamlit import cache
 from transformers import pipeline
@@ -6,21 +10,34 @@ cuda_device = 0 if torch.cuda.is_available() else -1
 
 
 @cache(allow_output_mutation=True)
-def load_model(txt_length):
-    """Load the AI model and check text length"""
-    if txt_length <= 8:
+def load_model(max_length: int):
+    """
+    Load the ML model with specified max length
+
+    :param max_length: output length of generated text
+    :return: pipeline with model for text generation
+    """
+
+    if max_length <= 8:
         return None
 
     return pipeline(
         "summarization",
         "pszemraj/long-t5-tglobal-base-16384-book-summary",
-        max_length=txt_length,
+        max_length=max_length,
         device=cuda_device
     )
 
 
-def generate_text(generation_text, generation_len):
-    """Function where AI generate short text"""
+def generate_text(generation_text: str, generation_len: int):
+    """
+    Generate text until the output length (which includes the context length) reaches generation_len
+
+    :param generation_text (str): source text to be processed
+    :param generation_len (int): output length
+    :return: generated text
+    """
+
     if len(generation_text) < 10:
         raise ValueError("Source text is too short")
     if generation_len <= 8:
